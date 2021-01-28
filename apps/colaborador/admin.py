8239ca0a5598b0579b0a0aa2d6fee478e440e5c9
marcelogumercinocosta <marcelogc@gmail.com>
2021-01-27 21:25:52 -0300

    remove bug snap in monitoramento Storage

--- apps/colaborador/admin.py
+++ apps/colaborador/admin.py
@@ -54,7 +54,7 @@ class ColaboradorAdmin(admin.ModelAdmin):
                 ("Informações Profissionais", {"fields": ["vinculo", "predio", "divisao", "ramal", "responsavel", "registro_inpe", "empresa", "data_inicio", "data_fim"]}),
                 ("Informações Portal", {"fields": ["username", "uid", "is_superuser", "is_staff", "is_active", "last_login", "date_joined"]}),
             ]
-            inlines = [GroupInLine, ColaboradorGrupoAcessoInLineRead]
+            self.inlines = [GroupInLine, ColaboradorGrupoAcessoInLineRead]
             self.readonly_fields = ["username", "uid", "is_staff", "is_active", "last_login", "date_joined"]
         return super(ColaboradorAdmin, self).change_view(request, object_id, form_url, extra_context)
 
